c0bb981aa55d48b175e120b244808731a268e2f4
neojarvis <techadmin@iamneo.ai>
2025-04-02 06:11:14 +0000

    Auto-save-commit-4/2/2025--11:41:08-AM

--- sample.py
+++ sample.py
@@ -1,2 +1,9 @@
 import mysql.connector
-print("MYSQL connector is installed")
+print("mysql connector is installed")
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user='root',
+    password='root'
+)
+cur = connection.cursor()
